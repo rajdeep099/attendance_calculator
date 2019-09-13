e201ffe440b3f33e07e31283eb2bfe01a1e03a60
rajdeep099 <55257345+rajdeep099@users.noreply.github.com>
2019-09-13 19:13:41 +0530

    updated
    changing of button colors, problem of flushing of output is solved .

--- attendance_backup.py
+++ attendance_backup.py
@@ -7,7 +7,8 @@ def show1():
    ta = int(ta)
    percent = (ta/tl)*100
       
-   Label(root, text='%.2f'%percent).grid(row=2, column=1)
+   la = Label(f, text='%.2f'%percent)
+   la.grid(row=2, column=1)
    return percent
 
 def show2():
@@ -29,7 +30,8 @@ def show2():
          
       elif percent >= rp:
          break
-   Label(root, text=i-1).grid(row=5,column=1)
+   lb = Label(f, text=i-1)
+   lb.grid(row=5,column=1)
 
 def show3():
    percent = 0
@@ -52,6 +54,8 @@ def show3():
    p2 = (pd/nlw)*100
    
    while True:
+       lc = Label(f, text="")
+       lc.grid(row=8, column=1)
        if (pd + ad) == nlw and p2 > rp and percent < rp:
           
           week = 0
@@ -69,10 +73,14 @@ def show3():
                 else:
                    break            
              
-          Label(root, text = 'Within {} week '.format(week)).grid(row = 8, column = 1)
-    
+          ld = Label(f, text = 'Within {} week '.format(week))
+          ld.grid(row = 8, column = 1)
+          break
+         
        elif (pd + ad) == nlw and p2 <= rp and percent < rp:
-          Label(root, text='not possible').grid(row=8,column=1)
+          le = Label(f, text="can't be done")
+          le.grid(row=8,column=1)
+          break
     
        elif (pd + ad) == nlw and percent > rp:
     
@@ -91,41 +99,75 @@ def show3():
                 else:
                    break            
              
-          Label(root, text = 'Within {} week '.format(week)).grid(row = 8, column = 1)
-    
+          lf = Label(f, text = 'Within {} week '.format(week))
+          lf.grid(row = 8, column = 1)
+          break
+         
        elif percent == rp:
           week = 0
-          Label(root, text = 'Within {} week '.format(week)).grid(row = 8, column = 1)
-    
+          lg = Label(f, text = 'Within {} week '.format(week))
+          lg.grid(row = 8, column = 1)
+          break
+         
        elif (pd + ad) != nlw:
-          Label(root, text = 'wrong input given').grid(row = 8, column = 1)
+          lh = Label(f, text = 'wrong input given')
+          lh.grid(row = 8, column = 1)
+          break
 
-if __name__ == __main__ :  
+# changing of colors
+def on_enterB1(e) :
+   b1['background'] = 'red'
+def on_enterB2(e) :
+   b2['background'] = 'green'
+def on_enterB3(e) :
+   b3['background'] = 'green'
+def on_enterB4(e) :
+   b4['background'] = 'green'
+def on_leave(e):
+    b1['background'] = 'SystemButtonFace'
+    b2['background'] = 'SystemButtonFace'
+    b3['background'] = 'SystemButtonFace'
+    b4['background'] = 'SystemButtonFace'
+
+# main
+if __name__ == "__main__" :  
     root = Tk()
-    Label(root, text="Total lectures").grid(row=0)
-    Label(root, text="Total attended").grid(row=1)
-    Label(root, text='Running Percent').grid(row=2)
-    Label(root, text='''Percent you
+    
+    root.title("Attendance Calculator")
+
+    # declare Frame as f
+    f = Frame(root, height = 290, width = 350)
+    f.propagate(0)
+    f.pack()
+    
+    # declaring Labels
+    l1 = Label(f, text="Total lectures")
+    l1.grid(row=0)
+    l2 = Label(f, text="Total attended")
+    l2.grid(row=1)
+    l3 = Label(f, text='Running Percent')
+    l3.grid(row=2)
+    l4 = Label(f, text='''Percent you
     require''').grid(row=3)
-    Label(root, text='''No. of lectures
-    per week''').grid(row = 4)
-    Label(root, text='''No. of lectures
+    l5 = Label(f, text='''No. of lectures
+    per week''').grid(row = 4) 
+    l6 = Label(f, text='''No. of lectures
     required
     (if daily attended)''').grid(row = 5)
-    Label(root, text='''Present days
+    l7 = Label(f, text='''Present days
     per week''').grid(row = 6)
-    Label(root, text='''Absent days
+    l8 = Label(f, text='''Absent days
     per week''').grid(row = 7)
-    Label(root, text='''no of weeks
+    l9 = Label(f, text='''no of weeks
     required''').grid(row = 8)
     
-    
-    total_lectures = Entry(root)
-    total_attended = Entry(root)
-    req_percent = Entry(root)
-    NoOfLecturesPerWeek = Entry(root)
-    PresentDays = Entry(root)
-    AbsentDays = Entry(root)
+    # taking entries
+    total_lectures = Entry(f)
+    total_attended = Entry(f)
+    req_percent = Entry(f)
+    NoOfLecturesPerWeek = Entry(f)
+    PresentDays = Entry(f)
+    AbsentDays = Entry(f)
     
     t = 0
     total_lectures == total_lectures.get()
@@ -137,11 +179,26 @@ if __name__ == __main__ :
     NoOfLecturesPerWeek.grid(row=4, column=1)
     PresentDays.grid(row=6, column=1)
     AbsentDays.grid(row=7, column=1)
+
     
-    Button(root, text='Quit', command=quit, activebackground = '#f00000').grid(row=9, column=1, sticky=W, pady=4)
+
+    # Buttons
+    b1 = Button(f, text='Quit', command=quit, activebackground = '#f00000')
+    b1.grid(row=9, column=1)
+    b1.bind("<Enter>", on_enterB1)
+    b1.bind("<Leave>", on_leave)
+
+    b2 = Button(f, text='Show', command=show1)
+    b2.grid(row=2, column=2)
+    b2.bind("<Enter>", on_enterB2)
+    b2.bind("<Leave>", on_leave)
+   
+    b3 = Button(f, text='Show', command=show2)
+    b3.grid(row=5, column=2)
+    b3.bind("<Enter>", on_enterB3)
+    b3.bind("<Leave>", on_leave)
     
-    Button(root, text='Show', command=show1).grid(row=2, column=2, sticky=W, pady=4)
-    
-    Button(root, text='Show', command=show2).grid(row=5, column=2, sticky=W, pady=4)
-    
-    Button(root, text='Show',command = show3).grid(row=8, column=2,sticky=W, pady=4)
+    b4 = Button(f, text='Show',command = show3)
+    b4.grid(row=8, column=2)
+    b4.bind("<Enter>", on_enterB4)
+    b4.bind("<Leave>", on_leave)
